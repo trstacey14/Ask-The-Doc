79738d5f25a5e617a8c4ab9368291aed25d17b10
trstacey14 <135732224+trstacey14@users.noreply.github.com>
2024-04-28 12:04:48 -0500

    Update RAG_Streamlit_app.py

--- RAG_Streamlit_app.py
+++ RAG_Streamlit_app.py
@@ -19,31 +19,29 @@ def generate_response(uploaded_file, openai_api_key, query_text):
         # Create retriever interface
         retriever = db.as_retriever()
         # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key),
-                                         chain_type='stuff', retriever=retriever)
+        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
         return qa.run(query_text)
 
+
 # Page title
-st.set_page_config(page_title='🦜🦜 Ask the Doc App')
-st.title('🦜🦜 Ask the Doc App')
+st.set_page_config(page_title='🦜🔗 Ask the Doc App')
+st.title('🦜🔗 Ask the Doc App')
 
 # File upload
 uploaded_file = st.file_uploader('Upload an article', type='txt')
-
 # Query text
-placeholder = 'Please provide a short summary'
-query_text = st.text_area('Enter your question:', value=placeholder if uploaded_file is None else '', disabled=not uploaded_file)
-
+query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
 # Form input and query
 result = []
 with st.form('myform', clear_on_submit=True):
     openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
-    submitted = st.form_submit_button('Submit', disabled=not (uploaded_file and query_text))
+    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
             response = generate_response(uploaded_file, openai_api_key, query_text)
             result.append(response)
-            del openai_api_key  # Assuming you want to delete the API key after use
+            del openai_api_key
+
 if len(result):
-    st.info(result[0])
+    st.info(response)
